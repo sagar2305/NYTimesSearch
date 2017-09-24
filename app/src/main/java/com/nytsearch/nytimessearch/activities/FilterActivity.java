5c9e4a4d1aee0ec6c41e7d152a972ae548a8c711
Sagar Mutha <sagar2305@gmail.com>
2017-09-24 11:38:51 -0700

    implemented the date picker

--- app/src/main/java/com/nytsearch/nytimessearch/activities/FilterActivity.java
+++ app/src/main/java/com/nytsearch/nytimessearch/activities/FilterActivity.java
@@ -1,12 +1,23 @@
 package com.nytsearch.nytimessearch.activities;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.nytsearch.nytimessearch.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class FilterActivity extends AppCompatActivity {
+
+    TextView tvDate;
+    Calendar myCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +26,43 @@ public class FilterActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        setupViews();
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        tvDate.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    private void setupViews() {
+        myCalendar = Calendar.getInstance();
+
+        tvDate = findViewById(R.id.tvDateValue);
+        updateLabel();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+//                task.setDueDate(myCalendar.getTime());
+                updateLabel();
+            }
+
+        };
+
+        tvDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(FilterActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
     }
 
 }
